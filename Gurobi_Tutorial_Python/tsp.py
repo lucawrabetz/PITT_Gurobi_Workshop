6ce91809b023fa36f9b943391381eebf81b4e918
lucawrabetz <wrabetzluca@gmail.com>
2020-10-16 15:22:59 -0400

    tsp with blanks

--- Gurobi_Tutorial_Python/tsp.py
+++ Gurobi_Tutorial_Python/tsp.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python
-
-# Copyright 2017, Gurobi Optimization, Inc.
-
-# Solve a traveling salesman problem on a randomly generated set of
-# points using lazy constraints.   The base MIP model only includes
-# 'degree-2' constraints, requiring each node to have exactly
-# two incident edges.  Solutions to this model may contain subtours -
-# tours that don't visit every city.  The lazy constraint callback
-# adds new constraints to cut them off.
-
 import sys
 import math
 import random
@@ -20,17 +9,16 @@ n=100#NUMBER OF CITIES
 # Callback - use lazy constraints to eliminate sub-tours
 
 def subtourelim(model, where):
-    if where == GRB.Callback.MIPSOL:
+    # when does the callback act? 
+
         # make a list of edges selected in the solution
-        vals = model.cbGetSolution(model._vars)
-        selected = tuplelist((i,j) for i,j in model._vars.keys() if vals[i,j] > 0.5)
+
         # find the shortest cycle in the selected edge list
-        tour = subtour(selected)
-        if len(tour) < n:
+
+        # check that subtour != all nodes
+        
             # add subtour elimination constraint for every pair of cities in tour
-            model.cbLazy(quicksum(model._vars[i,j]
-                                  for i,j in itertools.combinations(tour, 2))
-                         <= len(tour)-1)
+            
 
 
 # Given a tuplelist of edges, find the shortest subtour
@@ -50,58 +38,31 @@ def subtour(edges):
             cycle = thiscycle
     return cycle
 
-
-# Parse argument
-
-#if len(sys.argv) < 2:
-#    print('Usage: tsp.py npoints')
-#    exit(1)
-#n = int(sys.argv[1])
-
 # Create n random points
 
-random.seed(1)#fixes the seed, this guarantees the same output on each run
+random.seed(1) #fixes the seed, this guarantees the same output on each run
 points = [(random.randint(0,100),random.randint(0,100)) for i in range(n)]
 
-# Dictionary of Euclidean distance between each pair of points
 
+# Dictionary of Euclidean distance between each pair of points
 dist = {(i,j) :
     math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
     for i in range(n) for j in range(i)}
 
-m = Model()
+# Create Model
 
-# Create variables
+# Create variables - remember the opposite edge!
 
-vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
-for i,j in vars.keys():
-    vars[j,i] = vars[i,j] # edge in opposite direction
 
-# You could use Python looping constructs and m.addVar() to create
-# these decision variables instead.  The following would be equivalent
-# to the preceding m.addVars() call...
-#
-# vars = tupledict()
-# for i,j in dist.keys():
-#   vars[i,j] = m.addVar(obj=dist[i,j], vtype=GRB.BINARY,
-#                        name='e[%d,%d]'%(i,j))
+
 
 
 # Add degree-2 constraint
 
-m.addConstrs(vars.sum(i,'*') == 2 for i in range(n))
-
-# Using Python looping constructs, the preceding would be...
-#
-# for i in range(n):
-#   m.addConstr(sum(vars[i,j] for j in range(n)) == 2)
-
 
 # Optimize model
 
-m._vars = vars
-m.Params.lazyConstraints = 1
-m.optimize(subtourelim)
+
 
 vals = m.getAttr('x', vars)
 selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.5)
